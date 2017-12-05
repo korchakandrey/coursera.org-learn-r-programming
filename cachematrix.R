0ab7b041ddcf4f3b2783a2114319894f189b8254
Andrii Korchak <bestuser@mail.ru>
2017-12-05 11:48:59 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,63 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- matrix(ncol = dim(x)[2], nrow =dim(x)[1] )
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
+  get <- function(){ x } 
+  getsolve <- function(){ m }
+  setsolve <- function(){ if( det(x) != 0 ) m <<- solve(x)  
+                          else {
+                            message("This matrix do not have inverse!")
+                            m<<-NA
+                          }
+                        }
 
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!all(is.na(m))) {
+    message("getting cached data")
+    return(m)
+  }
+  x$setsolve()
+  message("getting calculated data")
+  x$getsolve()
 }
+
+
+
+#Checks 
+# The first check
+        m1<-matrix(1:16,nrow = 4)
+        mc1<-makeCacheMatrix( m1 )
+        cachesolve(mc1)
+        #This matrix do not have inverse!
+        #[1] NA
+        ## This is correct because matrix has zero discriminant( do not have inverse )
+        
+# The Second Check
+        m2 <- matrix(rnorm(16,4,2),ncol = 4)
+        mc2<-makeCacheMatrix( m2 )
+        cachesolve(mc2)
+        # [1] inverse matrix returned
+
+        cachesolve(mc2)
+        # getting cached data
+        # [1] inverse matrix returned
+
+# The Third Example( very big martix ) - to check productivity
+        m3  <- matrix(rnorm(16000000,67,23), nrow = 4000, byrow = TRUE)
+        ms3 <- makeCacheMatrix(m3)
+        cachesolve(ms3)
+        #takes some time to compute
+        
+        cachesolve(ms3)
+        #computes almost immediatly
